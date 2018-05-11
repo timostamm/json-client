d139b2c3cfbdd0491c4ff65362fcc02681f05d4a
Timo Stamm <ts@timostamm.de>
2018-05-11 16:59:26 +0200

    Initial commit.

--- README.md
+++ README.md
@@ -1,5 +1,57 @@
 # json-client
 
-A simple web client for JSON APIs using Guzzle and the symfony serializer.
+A simple web client for JSON APIs using Guzzle and the 
+symfony serializer.
+
+To implement a API client, you would extend AbstractApiClient 
+and write your methods, using the Guzzle Http Client to transmit.
 
 
+```PHP
+class MyClient extends AbstractApiClient {
+    
+    
+    protected function getBaseUri():string
+    {
+        return 'http://example.com/api/';
+    }
+    
+    
+    /**
+     * @throws ServerMessageException
+     * @throws UnexpectedResponseException
+     * @throws TransferException
+     */
+    public function send(Model $model):void
+    {
+        // The data will automatically be 
+        // serialized to JSON. 
+        $this->http->post('model', [
+            'data' => $model
+        ]);
+    }
+    
+    
+    /**
+     * @param int $id
+     * @throws ServerMessageException
+     * @throws UnexpectedResponseException
+     * @throws TransferException
+     * @returns Model 
+     */
+    public function get(int $id):Model
+    {
+        $response = $this->http->get('model/'.$id);
+        return $this->deserializeResponse($response, Model::class);
+    }
+
+
+}
+```
+
+So the implementation is up to you. 
+
+If you want more magic, have a look at [guzzle/command](https://github.com/guzzle/command).
+
+The abstract base class just adds some middleware to automatically 
+serialize data and to detect server error messages in JSON format.
--- src/AbstractApiClient.php
+++ src/AbstractApiClient.php
@@ -8,7 +8,116 @@
 
 namespace TS\Web\JsonClient;
 
-class AbstractApiClient
+
+use GuzzleHttp\Client;
+use GuzzleHttp\HandlerStack;
+use GuzzleHttp\RequestOptions;
+use Psr\Http\Message\ResponseInterface;
+use Symfony\Component\Serializer\SerializerInterface;
+use TS\Web\JsonClient\Exception\UnexpectedResponseException;
+use TS\Web\JsonClient\Middleware\SerializeRequestBodyMiddleware;
+use TS\Web\JsonClient\Middleware\ServerMessageMiddleware;
+
+
+abstract class AbstractApiClient
 {
+
+    private $handlerStack;
+    private $serializer;
+    protected $http;
+
+
+    public function __construct(  SerializerInterface $serializer )
+    {
+        $this->serializer = $serializer;
+        $this->handlerStack = $this->createHandlerStack();
+        $this->http = $this->createClient($this->handlerStack);
+    }
+
+
+    protected function createClient(HandlerStack $handlerStack):Client
+    {
+        return new Client([
+            'handler' => $handlerStack,
+            'base_uri' => $this->getBaseUri(),
+            RequestOptions::ALLOW_REDIRECTS => false,
+            RequestOptions::TIMEOUT => $this->getDefaultTimeout(),
+            RequestOptions::VERIFY => true,
+            RequestOptions::COOKIES => false,
+            RequestOptions::HEADERS => [
+                'User-Agent' => $this->getUserAgent(),
+                'Accept-Encoding' => 'gzip',
+                'Accept' => 'application/json'
+            ]
+        ]);
+    }
+
+
+    protected function createHandlerStack():HandlerStack
+    {
+        $stack = HandlerStack::create();
+
+        $stack->push(function(callable $handler) {
+            return new ServerMessageMiddleware($handler);
+        }, 'error_message');
+
+        $stack->push(function(callable $handler) {
+            return new SerializeRequestBodyMiddleware($handler, $this->serializer);
+        }, 'serialize_request_body');
+
+        return $stack;
+    }
+
+
+    abstract protected function getBaseUri():string;
+
+
+    protected function getDefaultTimeout():float
+    {
+        return 2.0;
+    }
+
+
+    protected function getUserAgent():string
+    {
+        return 'AbstractApiClient';
+    }
+
+
+    protected function expectResponseType(string $type, ResponseInterface $response):void
+    {
+        $actual = $response->getHeaderLine('Content-Type');
+        if ($actual !== $type) {
+            $msg = sprintf('Expected response content type to be %s, got %s instead.', $type, $actual);
+            throw new UnexpectedResponseException($msg, $response);
+        }
+    }
+
+
+    /**
+     * @param ResponseInterface $response
+     * @param string $type
+     * @param array $context
+     * @return mixed
+     */
+    protected function deserializeResponse(ResponseInterface $response, string $type, array $context=[])
+    {
+        $this->expectResponseType('application/json', $response);
+
+        $data = $response->getBody()->getContents();
+
+        try {
+
+            $object = $this->serializer
+                ->deserialize($data, $type, 'json', $context);
+
+            return $object;
+
+        } catch (\Exception $exception) {
+            $msg = sprintf('Failed to deserialize response body to type %s: %s', $type, $exception->getMessage());
+            throw new UnexpectedResponseException($msg, $response, $exception);
+        }
+    }
+
 
 }
